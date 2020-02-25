abc31ae799e2ddb02b16cc2caae2f4cc37b9f500
B Severino <biancasev@gmail.com>
2020-02-25 11:46:00 -0500

    Model for foster family data

---
+++ server/api/families/family-helper.js
@@ -0,0 +1,46 @@
+const db = require('../../../data/db-config.js')
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  remove,
+}
+
+function add(familyData) {
+  return db('families').insert(familyData)
+}
+
+function find() {
+  return db('families')
+}
+
+function findBy(filter) {
+  return db('families').where(filter)
+}
+
+function findById(id) {
+  return db('families')
+    .where('id', id)
+    .first()
+}
+
+function update(id, data) {
+  return db('families')
+    .where('id', id)
+    .update({
+      first_name: data.first_name,
+      last_name: data.last_name,
+      email: data.email,
+      phone: data.phone,
+      address: data.address,
+    })
+}
+
+function remove(id) {
+  return db('families')
+    .where('id', id)
+    .del()
+}
