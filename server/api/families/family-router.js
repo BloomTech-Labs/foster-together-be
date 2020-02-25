ee5d3e49abe4e6cffaaf7c1923afb35b346cda95
jarrodskahill <jarrodskahill@gmail.com>
2020-02-25 10:04:51 -0800

    added some routes to family router

--- server/api/families/family-router.js
+++ server/api/families/family-router.js
@@ -1,14 +1,40 @@
-// const router = require('express').Router()
-// const Family = './familyHelper.js'
+const router = require('express-promise-router')()
 
-// router.get('/families', (req, res) => {
-//   Family.getFamilies()
-//     .then(fam => {
-//       res.json(fam)
-//     })
-//     .catch(error => {
-//       res.status(500).json({ error: "Couldn't get familtes" })
-//     })
-// })
+const Families = require('./family-helper.js')
 
-// module.exports = router
+router.post('/', async (req, res) => {
+  let family = req.body
+  const saved = await Families.add(family)
+  res.status(201).json({ message: 'Family successfully added.', saved })
+})
+
+router.get('/', async (req, res) => {
+  const family = await Families.find()
+  res.status(200).json(user)
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const family = await Families.findById(id)
+  res.status(200).json(family)
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const family = req.body
+  const updated = await Families.update(id, family)
+  res.status(201).json(updated)
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  const deleted = await Families.remove(id)
+  res.status(200).json({ message: 'family successfully deleted.', deleted })
+})
+
+router.use((err, req, res, next) =>
+  res.status(500).json({
+    message: 'Uh Oh! 500 Error!',
+    error: err.message.replace(/\\/g, ''),
+  })
+)
