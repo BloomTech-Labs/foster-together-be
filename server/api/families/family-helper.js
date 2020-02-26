a3ad64d907daf92a945226739519a5debd10f654
B Severino <biancasev@gmail.com>
2020-02-25 19:23:24 -0500

    Added city_state_zip_id generation to family-helper.js

--- server/api/families/family-helper.js
+++ server/api/families/family-helper.js
@@ -10,13 +10,58 @@ module.exports = {
 }
 
 function add(familyData) {
-  return db('families').insert({ ...familyData, city_state_zip_id: 1 }, [
-    'first_name',
-    'last_name',
-    'email',
-    'phone',
-    'address',
-  ])
+  let city = await Locations.findCityByName(familyData.city)
+  let state = await Locations.findStateByName(familyData.state)
+  let zip = await Locations.findByZip(Number(familyData.zip))
+
+  if (!city) {
+    let newCity = await Locations.addCity({ city: familyData.city })
+    city = newCity[0]
+  }
+
+  if (!state) {
+    let newState = await Locations.addState({ state: familyData.state })
+    state = newState[0]
+  }
+
+  if (!zip) {
+    let newZip = await Locations.addZip({ zip: Number(familyData.zip) })
+    zip = newZip[0]
+  }
+
+  let cityStateZip = await Locations.findByLocation(
+    city.city_id,
+    state.state_id,
+    zip.zip_id
+  )
+
+  if (!cityStateZip) {
+    let newCityStateZip = await Locations.addCityStateZip(
+      city.city_id,
+      state.state_id,
+      zip.zip_id
+    )
+    cityStateZip = newCityStateZip[0]
+  }
+
+  return db('families').insert(
+    {
+      first_name: familyData.first_name,
+      last_name: familyData.last_name,
+      email: familyData.email,
+      phone: familyData.phone,
+      address: familyData.address,
+      city_state_zip_id: cityStateZip.city_state_zip_id,
+    },
+    [
+      'first_name',
+      'last_name',
+      'email',
+      'phone',
+      'address',
+      'city_state_zip_id',
+    ]
+  )
 }
 
 function find() {
