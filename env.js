cde279c79911916a1155f44f8e615992d3475595
B Severino <biancasev@gmail.com>
2020-02-27 13:11:40 -0500

    Removed https and fs modules, development just running on http now

--- env.js
+++ env.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV, // set to production
-  SSL_PASSPHRASE: process.env.SSL_PASSPHRASE, // set when generating OpenSSL certificate
   DATABASE_URL: process.env.DATABASE_URL, // production DB
   TEST_DB: process.env.TEST_DB, // test DB
 }
