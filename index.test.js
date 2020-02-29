e38bbd3b8e128cc373573d2ba9a69cefd028e5d3
michelangelo17 <michelangelo.markus@gmail.com>
2020-02-28 19:31:39 -0800

    trying with aws test database

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
 describe('test environment', () => {
   test('database enviroment variable is set to "testing"', () =>
-    expect(process.env.NODE_ENV).toBe('testing'))
+    expect(process.env.NODE_ENV).toBe('test'))
 })
