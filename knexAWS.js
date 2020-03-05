f9e908029acf691339005b2378fae04bf35e44bb
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-05 03:02:26 -0800

    staged and commited untracked

--- knexAWS.js
+++ knexAWS.js
@@ -1,6 +1,6 @@
 const { RDS_HOSTNAME, RDS_PASSWORD, RDS_USERNAME, RDS_PORT } = require('./env')
 
-export const awsDB = {
+module.exports = awsDB = {
   host: RDS_HOSTNAME,
   user: RDS_USERNAME,
   password: RDS_PASSWORD,
--- knextest.js
+++ knextest.js
@@ -1,9 +1,15 @@
-const {HOSTNAME_TEST, USERNAME_TEST, PASSWORD_TEST, PORT_TEST, NAME_TEST} = require('./env')
+const {
+  HOSTNAME_TEST,
+  USERNAME_TEST,
+  PASSWORD_TEST,
+  PORT_TEST,
+  NAME_TEST,
+} = require('./env')
 
-export const testDB =  {
+module.exports = testDB = {
   host: HOSTNAME_TEST,
   user: USERNAME_TEST,
   password: PASSWORD_TEST,
   port: PORT_TEST,
   database: NAME_TEST,
-},
+}
