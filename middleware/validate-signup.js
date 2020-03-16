89b74525413311ac26746305a30ec4c00b40f063
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-16 15:16:05 -0700

    validate signup refactored using arrays

--- middleware/validate-signup.js
+++ middleware/validate-signup.js
@@ -1,25 +1,26 @@
 function validateSignup(req, res, next) {
   const body = req.body
-
-  if (Object.keys(body).length === 0)
-    return res.status(400).json({ message: 'Missing data.' })
-  if (!body.first_name)
-    return res.status(400).json({ message: 'first_name is required.' })
-  if (!body.last_name)
-    return res.status(400).json({ message: 'last_name is required.' })
-  if (!body.email)
-    return res.status(400).json({ message: 'email is required.' })
-  if (!body.phone)
-    return res.status(400).json({ message: 'phone is required.' })
-  if (!body.address)
-    return res.status(400).json({ message: 'addreturn ress is required.' })
-  if (!body.city) return res.status(400).json({ message: 'city is required.' })
-  if (!body.state)
-    return res.status(400).json({ message: 'state is required.' })
-  if (!body.zip) return res.status(400).json({ message: 'zip is required.' })
+  const keyArray = Object.keys(body)
+  const expectedKeys = [
+    'first_name',
+    'last_name',
+    'email',
+    'phone',
+    'address',
+    'city',
+    'state',
+    'zip',
+  ]
+  const difference = keyArray
+    .filter(x => !expectedKeys.includes(x))
+    .concat(expectedKeys.filter(x => !keyArray.includes(x)))
+  if (keyArray.length === 0) res.status(400).json({ message: 'Missing data.' })
+  if (difference.length > 0)
+    return res
+      .status(400)
+      .json({ message: difference.map(key => `${key} is required`) })
   if (body.zip.length !== 5)
-    return res.status(400).json({ message: 'zip must be 5 characters.' })
-
+    res.status(400).json({ message: 'zip must be 5 characters.' })
   next()
 }
 
