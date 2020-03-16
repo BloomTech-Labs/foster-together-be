9069ba956e14dacc1d92f8de38c2ee0604f583f5
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-16 13:27:54 -0700

    refactored validate-signup, changed code climate settings

--- middleware/validate-signup.js
+++ middleware/validate-signup.js
@@ -1,29 +1,26 @@
 function validateSignup(req, res, next) {
   const body = req.body
 
-  if (Object.keys(body).length === 0) {
-    res.status(400).json({ message: 'Missing data.' })
-  } else if (!body.first_name) {
-    res.status(400).json({ message: 'first_name is required.' })
-  } else if (!body.last_name) {
-    res.status(400).json({ message: 'last_name is required.' })
-  } else if (!body.email) {
-    res.status(400).json({ message: 'email is required.' })
-  } else if (!body.phone) {
-    res.status(400).json({ message: 'phone is required.' })
-  } else if (!body.address) {
-    res.status(400).json({ message: 'address is required.' })
-  } else if (!body.city) {
-    res.status(400).json({ message: 'city is required.' })
-  } else if (!body.state) {
-    res.status(400).json({ message: 'address is required.' })
-  } else if (!body.zip) {
-    res.status(400).json({ message: 'zip is required.' })
-  } else if (body.zip.length !== 5) {
-    res.status(400).json({ message: 'zip must be 5 characters.' })
-  } else {
-    next()
-  }
+  if (Object.keys(body).length === 0)
+    return res.status(400).json({ message: 'Missing data.' })
+  if (!body.first_name)
+    return res.status(400).json({ message: 'first_name is required.' })
+  if (!body.last_name)
+    return res.status(400).json({ message: 'last_name is required.' })
+  if (!body.email)
+    return res.status(400).json({ message: 'email is required.' })
+  if (!body.phone)
+    return res.status(400).json({ message: 'phone is required.' })
+  if (!body.address)
+    return res.status(400).json({ message: 'addreturn ress is required.' })
+  if (!body.city) return res.status(400).json({ message: 'city is required.' })
+  if (!body.state)
+    return res.status(400).json({ message: 'state is required.' })
+  if (!body.zip) return res.status(400).json({ message: 'zip is required.' })
+  if (body.zip.length !== 5)
+    return res.status(400).json({ message: 'zip must be 5 characters.' })
+
+  next()
 }
 
 module.exports = validateSignup
