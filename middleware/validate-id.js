8e4af91bfe9b5d795584beae15fa714d08cc6233
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-16 22:20:21 -0700

    replaced family and neighbors with members

--- middleware/validate-id.js
+++ middleware/validate-id.js
@@ -3,16 +3,9 @@ const db = require('../data/db-config.js')
 function validateId() {
   return async function(req, res, next) {
     const { membertype, id } = req.params
-    let resource
-    if (membertype === 'neighbor') {
-      resource = await db('neighbors')
-        .first()
-        .where('neighbor_id', id)
-    } else {
-      resource = await db('families')
-        .first()
-        .where('family_id', id)
-    }
+    const resource = await db(membertype)
+      .first()
+      .where('id', id)
     if (!resource) {
       res.status(400).json({ message: 'Invalid ID.' })
     } else {
