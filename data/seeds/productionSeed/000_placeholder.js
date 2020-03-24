12f1ed0bc34a5dcfe1d885fd4b1ff163633fa6e7
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-23 17:48:08 -0700

    added untracked files

---
+++ data/seeds/productionSeed/000_placeholder.js
@@ -0,0 +1,7 @@
+// Probably don't want to seed database with anything
+// except a temporary admin user to create their admin accounts with,
+//and will only do that once, so for now set this to do nothing
+
+exports.seed = function() {
+  return
+}
