869b6e0fb64e1e2b0a1fa712db853c14118a3f83
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-24 21:25:26 -0700

    fixed codeclimate issue, reduced addApp function length

--- server/apply/model.js
+++ server/apply/model.js
@@ -1,26 +1,37 @@
 const db = require('../../data/db-config.js')
 
-const addApp = async (member_id, values) => {
+const q2 = async values => {
   let app_q2 = await db('app_q2')
     .where(values.app_q2)
     .first()
   if (!app_q2) app_q2 = (await db('app_q2').insert(values.app_q2, ['id']))[0]
+  return app_q2
+}
 
-  const app_q3 = await db('app_q3')
+const q3 = async values =>
+  await db('app_q3')
     .where({ answer: values.app_q3 })
     .first()
 
-  const app_q4 = await db('app_q4')
+const q4 = async values =>
+  await db('app_q4')
     .where(values.app_q4)
     .first()
 
-  const app_q5 = await db('app_q5')
+const q5 = async values =>
+  await db('app_q5')
     .where({ answer: values.app_q5 })
     .first()
 
-  const app_approved = await db('app_approved')
-    .where({ approved: false })
-    .first()
+const addApp = async (member_id, values) => {
+  const app_q2 = await q2(values)
+
+  const app_q3 = await q3(values)
+
+  const app_q4 = await q4(values)
+
+  const app_q5 = await q5(values)
+
   return (
     await db('application').insert(
       {
@@ -32,7 +43,7 @@ const addApp = async (member_id, values) => {
         app_q6: values.app_q6,
         app_q7: values.app_q7,
         app_q8: values.app_q8,
-        app_approved_id: app_approved.id,
+        app_approved_id: 1,
         member_id,
       },
       ['id']
