21aecfc721bf7b300eca4c4d166d5a77f6193b3a
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-27 15:16:55 -0700

    updated to return application status with members

--- server/members/member-helper.js
+++ server/members/member-helper.js
@@ -68,16 +68,8 @@ async function add(membertype, data) {
   return member_id
 }
 
-const appStatus = async m_id => {
-  const status = await db('application')
-    .where('member_id', m_id)
-    .first()
-  if (!status) return 1
-  return status.app_approved_id
-}
-
-async function find(filter) {
-  const members = await db('users AS u')
+const getMembers = async filter =>
+  await db('users AS u')
     .join('members AS m', 'u.member_id', 'm.id')
     .join('membertypes AS mt', 'm.membertype_id', 'mt.id')
     .join('city_state_zip AS csz', 'csz.id', `m.city_state_zip_id`)
@@ -98,14 +90,24 @@ async function find(filter) {
       'latitude',
       'type'
     )
-    .modify(function(queryBuilder) {
-      if (filter) queryBuilder.where(filter)
-    })
+    .modify(queryBuilder => filter && queryBuilder.where(filter))
+
+const appStatus = async m_id => {
+  const status = await db('application')
+    .where('member_id', m_id)
+    .first()
+  if (!status) return 1
+  return status.app_approved_id
+}
+
+async function find(filter) {
+  const members = await getMembers(filter)
 
   return await Promise.all(
     members.map(async member => ({
       ...member,
       application: member.type === 'families' ? 2 : await appStatus(member.id),
+      // same method could work for training and background check fields.
     }))
   )
 }
