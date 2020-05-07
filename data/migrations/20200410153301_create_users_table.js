66a9045fe931c81660878a989b60dd97f44df5fc
Tyler de Friess <tyler.defriess@gmail.com>
2020-05-07 15:33:12 -0400

    Cleaned up formatting per code critique review

--- data/migrations/20200410153301_create_users_table.js
+++ data/migrations/20200410153301_create_users_table.js
@@ -1,9 +1,5 @@
 // This file is not used due to a miscommunication and left in only so that github tests will pass.
 
-exports.up = function(knex) {
-    
-  };
+exports.up = function(knex) {};
 
-exports.down = function(knex) {
-    
-  };
+exports.down = function(knex) {};
